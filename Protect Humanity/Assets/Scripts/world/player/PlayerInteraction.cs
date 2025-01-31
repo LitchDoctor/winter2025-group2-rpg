2091a85522dfb088a38d121daffce20a8f807d72
Owen Wickman <ogw@halfangle.com>
2025-01-30 17:17:23 -0800

    merged world/overworld and playermovement scripts as well as added TextMeshPro text to world scene displaying palyerstats

---
+++ Protect Humanity/Assets/Scripts/world/player/PlayerInteraction.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public GameObject currentInterObj = null;
+    public InteractionObject currentInterObjectScript = null;
+
+    void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && currentInterObj){
+            currentInterObj.SendMessage ("DoInteraction");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag ("interObject")){
+            Debug.Log(other.name);
+            currentInterObj = other.gameObject;
+            currentInterObjectScript = currentInterObj.GetComponent <InteractionObject> ();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other )
+    {
+        if(other.CompareTag("interObject")){
+            if(other.gameObject == currentInterObj){
+                currentInterObj = null;
+            }
+        }
+    }
+}
