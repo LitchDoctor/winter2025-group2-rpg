1b477aaad6aea516d26c143871165e3114924811
Fujikaww <fujikaww@oregonstate.edu>
2025-02-04 17:03:42 -0800

    saving object to a value.

--- Protect Humanity/Assets/Scripts/world/player/PlayerInteraction.cs
+++ Protect Humanity/Assets/Scripts/world/player/PlayerInteraction.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
-    // public GameObject currentInterObj = null;
+    public GameObject currentInterObj = null;
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag ("interObject")){
             Debug.Log(other.name);
-            // currentInterObj = other.gameObject;
+            currentInterObj = other.gameObject;
         }
     }
 }
