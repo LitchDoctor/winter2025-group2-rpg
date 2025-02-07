1b06373cd46d5a7a354f2d510cd81b001fec032a
Fujikaww <fujikaww@oregonstate.edu>
2025-02-06 16:55:15 -0800

    Items can now be added to inventory and stored.

--- Protect Humanity/Assets/Scripts/world/HealthPotion/InteractionObject.cs
+++ Protect Humanity/Assets/Scripts/world/HealthPotion/InteractionObject.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class InteractionObject : MonoBehaviour
 {
-    //public bool inventory; //if true, object can be stored in inventory
+    public bool inventory; //if true, object can be stored in inventory
+    private Rigidbody2D rb;
 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
     public void DoInteraction()
     {
         //Picked up and put in inventory
