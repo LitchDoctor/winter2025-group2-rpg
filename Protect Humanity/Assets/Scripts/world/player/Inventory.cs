1080dad7732d0cef917c987190a48cc1e2a2d6b9
Fujikaww <fujikaww@oregonstate.edu>
2025-03-03 13:30:28 -0800

    Added comments to inventory file

--- Protect Humanity/Assets/Scripts/world/player/Inventory.cs
+++ Protect Humanity/Assets/Scripts/world/player/Inventory.cs
@@ -9,25 +9,28 @@ using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
-    // create the array to hold items
+    // Array to hold items
     public GameObject[] inventory = new GameObject [9];
     public GameObject InventoryUI;
 
-    public PlayerStats playerstats;
-    public Button equipped;
-    private GameObject EquippedItem;
-    public GameObject ItemInfoPanel;
-    public TextMeshProUGUI itemNameText;
-    public Image itemIcon;
-    public TextMeshProUGUI itemDescriptionText;
-    public Button equipButton;
 
-    private GameObject SelectedItem;
+    public PlayerStats playerstats;                        // Reference to player stats
+    public Button equipped;                                // Button to display equipped item
+    private GameObject EquippedItem;                       // Currently equipped item
+    public GameObject ItemInfoPanel;                       // UI panel for displaying item details
+    public TextMeshProUGUI itemNameText;                   // Text displaying item name
+    public Image itemIcon;                                 // Image displaying item
+    public TextMeshProUGUI itemDescriptionText;            // Text displaying item description
+    public Button equipButton;                             // Button to equip an item
+    private GameObject SelectedItem;                       // Currently selected item                   
+    [SerializeField] public Button[] itemSlots;            // Array of item slot buttons
 
-    [SerializeField] public Button[] itemSlots;
+
+
 
     public void Start ()
     {
+        // Hide inventory UI 
         InventoryUI.SetActive(false);
         // UpdateInventoryUI();
     }
@@ -38,7 +41,7 @@ public class Inventory : MonoBehaviour
         bool itemAdded = false;
 
 
-        // find the first open slot within inventory
+        // Find the first open slot within inventory
         for(int i = 0; i < inventory.Length; i++)
         {
             if(inventory [i] == null) 
@@ -51,7 +54,7 @@ public class Inventory : MonoBehaviour
         }
 
 
-        // if inventory is full
+        // If inventory is full
         if(!itemAdded)
         {
             Debug.Log ("Inventory full - item not added");
@@ -61,7 +64,7 @@ public class Inventory : MonoBehaviour
         UpdateInventoryUI();
     }
 
-
+    // Method to remove item from inventory
     public void RemoveItem(int slotIndex)
     {
         if(inventory[slotIndex] != null)
@@ -75,20 +78,23 @@ public class Inventory : MonoBehaviour
     }
 
 
+    // Method to update the inventory UI display
     void UpdateInventoryUI()
     {
         for(int i = 0; i < itemSlots.Length; i++){
             if(inventory[i] != null){
+                // Displays item name and sprites
                 itemSlots[i].GetComponentInChildren<TextMeshProUGUI>().text = inventory[i].name;
                 itemSlots[i].GetComponentInChildren<Image>().sprite = inventory[i].GetComponent<SpriteRenderer>().sprite;
             }
             else{
+                // Clear slot if empty 
                 itemSlots[i].GetComponentInChildren<TextMeshProUGUI>().text = "";
             }
         }
     }
 
-
+    // Method to toggle inventory UI on and off
     public void ToggleInventory()
     {
         bool isActive = !InventoryUI.activeSelf;
@@ -100,23 +106,27 @@ public class Inventory : MonoBehaviour
         }
     }
 
+    // Method to select an item from inventory
     public void SelectItem(int slot)
     {
         SelectedItem = inventory[slot];
 
         if(SelectedItem != null)
         {
+            // Display item details in the UI
             itemNameText.text = SelectedItem.name;
             itemIcon.sprite = SelectedItem.GetComponent<SpriteRenderer>().sprite;
             InteractionObject itemScript = SelectedItem.GetComponent<InteractionObject>();
 
             ItemInfoPanel.SetActive(true);
 
+            // Assign equip button functionality
             equipButton.onClick.RemoveAllListeners();
             equipButton.onClick.AddListener(() => equipItem());
             if (itemScript != null && itemDescriptionText != null)
             {
-                itemDescriptionText.text = itemScript.description; // Show item description
+                // Show item description
+                itemDescriptionText.text = itemScript.description; 
             }
             else
             {
@@ -125,31 +135,34 @@ public class Inventory : MonoBehaviour
         }
     }
     
-
+    // Method to equip the selected item    
     public void equipItem(){
         if (SelectedItem != null){
             EquippedItem = SelectedItem;
 
-            
+            // Update UI with equipped item details
             equipped.GetComponentInChildren<TextMeshProUGUI>().text = EquippedItem.name;
             equipped.GetComponentInChildren<Image>().sprite = EquippedItem.GetComponent<SpriteRenderer>().sprite;
             // int test = SelectedItem.GetComponent<Stats>().attack;
             
+            // Update player stats
             playerstats.UpdateStats();
         }
     }
 
+    // Method to unequip the currently equipped item
     public void UnequipItem(){
         EquippedItem = null;
 
+        // Reset equipped item UI
         equipped.GetComponentInChildren<TextMeshProUGUI>().text = "Equipped";
         equipped.GetComponentInChildren<Image>().sprite = null;
 
-
+        // Update player stats
         playerstats.UpdateStats();
     }
 
-
+    // Method to get stats of the currently equipped item
     public Stats getEquipped(){
         GameObject g = new GameObject();
         g.AddComponent<Stats>();
