1d8a864503f57f3e5f456756f1b11aacc81fa295
GrahamAtOSU <glaznerg@oregonstate.edu>
2025-03-11 14:08:38 -0700

    Added returnfrombattle script to player

---
+++ Protect Humanity/Assets/ReturnFromBattle.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class returnFromBattle : MonoBehaviour
+{
+    public SceneInfo SceneInfo;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+}
--- Protect Humanity/Assets/SceneInfo.cs
+++ Protect Humanity/Assets/SceneInfo.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 [CreateAssetMenu(fileName = "SceneInfo", menuName = "Scriptable Objects/SceneInfo")]
 public class SceneInfo : ScriptableObject
 {
     public bool returnedFromBattle = false;
+
+    public Vector3 returnPosition;
 }
